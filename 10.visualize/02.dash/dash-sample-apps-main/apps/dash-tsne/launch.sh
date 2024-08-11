dd0735d57030aa6d29853909c16690ecd5807f54
Omar Essam <142716851+engomaressam@users.noreply.github.com>
2024-08-11 08:49:37 +0300

    deleted

--- 10.visualize/02.dash/dash-sample-apps-main/apps/dash-tsne/launch.sh
+++
@@ -1,5 +0,0 @@
-wget -Nq https://github.com/plotly/datasets/raw/master/dash-sample-apps/dash-tsne/data.zip
-wget -Nq https://github.com/plotly/datasets/raw/master/dash-sample-apps/dash-tsne/demo_embeddings.zip
-unzip -nq ./data.zip
-unzip -nq ./demo_embeddings.zip
-gunicorn app:server --workers 4
