883171ba5362dac3b58db2cc09acc652e6c2ebcc
engomaressam <omar.essam@rowad-rme.com>
2024-09-04 10:46:24 +0300

    dddddd

---
+++ 13.django/omar_warehouse_1.1/ap_check/utils.py
@@ -0,0 +1,7 @@
+from django.db import connection
+
+def get_distinct_project_names():
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT DISTINCT project_name FROM ap_check")  # Adjust table name if needed
+        results = cursor.fetchall()
+    return [row[0] for row in results if row[0]]
